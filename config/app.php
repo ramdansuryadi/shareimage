d6147e588f5270e34fe66d6c9e345f5933eca673
ramdan suryadi <raamdansuryadi@gmail.com>
2015-12-28 11:33:01 +0700

    update

--- config/app.php
+++ config/app.php
@@ -146,6 +146,8 @@ return [
         App\Providers\RouteServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
 
+
+
     ],
 
     /*
@@ -195,7 +197,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
         'Image' => Intervention\Image\Facades\Image::class,
-
 
     ],
 
--- resources/views/pages/portofolio.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('master')
-
-@section('content')
-<h1>about me</h1>
-@stop
